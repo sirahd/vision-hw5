0fd7327afc423b02ba858d1ec5c9bcdf0f9a5be6
sirahd <sirah@uw.edu>
2018-05-17 16:00:26 -0700

    add data augmentation

--- dataloader.py
+++ dataloader.py
@@ -10,13 +10,15 @@ class CifarLoader(object):
 		transform = transforms.Compose(
 		    [
 		     # TODO: Add data augmentations here
+                     transforms.RandomHorizontalFlip(),
+                     transforms.RandomRotation(20),
 		     transforms.ToTensor(),
 		     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 		     ])
 
 		transform_test = transforms.Compose([
 		    transforms.ToTensor(),
-		    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)), 
+		    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
 		])
 
 		trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
@@ -25,9 +27,9 @@ class CifarLoader(object):
 		                                          shuffle=True, num_workers=2)
 
 		testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-		                                       download=True, transform=transform_test) 
+		                                       download=True, transform=transform_test)
 		self.testloader = torch.utils.data.DataLoader(testset, batch_size=args.batchSize,
 		                                         shuffle=False, num_workers=2)
 
 		self.classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-		
+
